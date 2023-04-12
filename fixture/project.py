2fa2ab2f91ef6b3737d45efb120e0993839df0f7
Lera <v.osh.frrr@gmail.com>
2023-04-12 22:29:42 +0300

    тест на удаление проекта

--- fixture/project.py
+++ fixture/project.py
@@ -27,6 +27,13 @@ class ProjectHelper:
         wd.find_element_by_name("view_state").send_keys(project.view_state)
         wd.find_element_by_name("description").send_keys(project.description)
 
+    def delete_project_by_id(self, id):
+        wd = self.app.wd
+        self.open_manage_projects_page()
+        wd.find_element_by_xpath("//a[@href='manage_proj_edit_page.php?project_id=%s']" % id).click()
+        wd.find_element_by_xpath("//input[@value='Delete Project']").click()
+        wd.find_element_by_xpath("//input[@value='Delete Project']").click()
+        self.open_manage_projects_page()
 
 
 
---
+++ tests/test_delete_project.py
@@ -0,0 +1,13 @@
+from model.project import Project
+import random
+
+
+def test_delete_project_by_id(app, db):
+    if len(db.get_project_list()) == 0:
+        app.project.create_project(Project(name='lala', status='development'))
+    old_projects = db.get_project_list()
+    random_project = random.choice(old_projects)
+    app.project.delete_project_by_id(random_project.id)
+    new_projects = db.get_project_list()
+    old_projects.remove(random_project)
+    assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
